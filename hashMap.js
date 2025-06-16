104aa0e42b45221443c24bc0bfac909b44d92f92
Ananth S Karnavar <41864363+Ananth-thE-CodER@users.noreply.github.com>
2025-06-16 15:36:28 +0530

    Hashmap starting

---
+++ hashMap.js
@@ -0,0 +1,22 @@
+class HashMap {
+    constructor(capacity=16, loadFactor=0.75) {
+        this.capacity = capacity;
+        this.loadFactor = loadFactor;
+        this.table = new Array(capacity);
+    }
+
+    hash(key) {
+        let hashCode = 0;
+        const primeNumber = 31;
+        for (let i = 0; i < key.length; i++) {
+            hashCode = (primeNumber * hashCode + key.charCodeAt(i)) % this.capacity;
+        }
+        return hashCode;
+    }
+
+    set(key, value) {
+        let bucket = this.hash(key);
+        this.table[bucket] = value; 
+    }
+
+}
