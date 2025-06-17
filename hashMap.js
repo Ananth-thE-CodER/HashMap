4bffd39854d9223de20bdc7c1e051b5a3715fece
Ananth <ananthskarnavar8470@gmail.com>
2025-06-17 21:49:18 +0530

    Working on Hashmap ---------------------------------- Working on re-hash operation.

--- hashMap.js
+++ hashMap.js
@@ -1,8 +1,11 @@
+import { LinkedList } from "./linkedList.js";
+
 class HashMap {
     constructor(capacity=16, loadFactor=0.75) {
         this.capacity = capacity;
         this.loadFactor = loadFactor;
-        this.table = new Array(capacity);
+        this.table = Array.from({ length: capacity }, () => new LinkedList());
+        this.bucketCount = 0;
     }
 
     hash(key) {
@@ -16,7 +19,37 @@ class HashMap {
 
     set(key, value) {
         let bucket = this.hash(key);
-        this.table[bucket] = value; 
+        let data = JSON.stringify({key, value});
+
+        if ((this.bucketCount / this.capacity) > this.loadFactor) {
+            this.reHash();
+        }
+        // Check if the bucket contains any data
+        if (this.table[bucket].getSize() > 0) { // Linear probing LL.
+            let index = this.table[bucket].find(data);
+            if (index) {
+                this.table[bucket].insertAt(data, index);  // This means that this is an update operation, not a collision.
+            }
+            else {
+                this.table[bucket].append(data);  // Collision
+            }
+        }
+        else {  // No data
+            this.table[bucket].append(data);
+            this.bucketCount++;
+        }
+    }
+
+    reHash() {
+        this.table.forEach((List) => {
+            let size = List.getSize()
+            if (size > 0) {
+                for (var i=0; i < size; i++) {
+                    // Store list items as array, then rehash. Also delete this comment.
+                    let head = List.head;
+                }
+            }
+        })
     }
 
 }
--- linkedList.js
+++ linkedList.js
@@ -1,6 +1,6 @@
 import { Node } from "./nodes.js";
 
-class LinkedList {
+export class LinkedList {
     constructor() {
         this.size = 0;
         this.head = null;
@@ -17,6 +17,62 @@ class LinkedList {
             this.head = node;
             this.head.next = head;
         }
+        this.size++;
+    }
+
+    append(value) {
+        let node = new Node(value);
+        if (!this.head) {
+            this.head = node;
+        }
+        else {
+            this.tail.next = node;
+        }
+        this.size++; 
+        this.tail = node;
+    }
+
+    getSize() {
+        return this.size;
+    }
+
+    find(value) {
+        let i = 0;
+        let found = false;
+        let index = null;
+        let node = this.head;
+        while (i <= this.size - 1) {
+            if (node.value == value) {
+                if (index == null) {
+                    index = 0;
+                }
+                found = true;
+                break;
+            }
+            node = node.next;
+            if (index == null) index = 0;
+            index++;
+        }
+        return index;
+    }
+
+    insertAt(value, index) {
+        if (index < 0) console.log("Please enter a positive index.")
+        let i = 1;
+        let node = this.head;
+        let newNode = new Node(value);
+        if (index == 0) {
+            newNode.next = this.head;
+            this.size++;
+            return;
+        }
+        while (i < index) {
+            node = node.next;
+            i++;
+        }
+        let nextNode = node.next;
+        node.next = newNode;
+        newNode.next = nextNode;
         this.size++;
     }
 }
